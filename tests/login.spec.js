06d6bc17f87e47c42cfd07458f3edfe15e2f1d64
jit55555 <jiteshghadi5@gmail.com>
2025-06-06 20:27:33 +0530

    commit new new

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -6,4 +6,5 @@ test('login test',async({page})=>{
     const lg = new login(page)
     await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
     await lg.loginMethod('Admin','admin123')
+    
 })
